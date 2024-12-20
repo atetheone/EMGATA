c891428024aaeb5a78c21cb9e04e2ebff4a82a1c
atetheone <atevirran@gmail.com>
2024-12-20 11:54:54 +0000

    Add Model class with properties for BrandId, Name, and Description

---
+++ EMGATA.API/Models/Model.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace EMGATA.API.Models;
+
+public class Model : BaseEntity
+{
+	[Required]
+	public int BrandId { get; set; }
+	[Required]
+	public string Name { get; set; }
+	public string Description { get; set; }
+
+	// Navigation properties
+	public virtual Brand Brand { get; set; }
+	public virtual ICollection<Car> Cars { get; set; }
+}
