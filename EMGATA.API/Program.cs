03997fafb711e68b4c00908b9a89f9ef8cee42ad
atetheone <atevirran@gmail.com>
2024-12-21 12:06:40 +0000

    Add JWT authentication configuration to Program.cs

--- EMGATA.API/Program.cs
+++ EMGATA.API/Program.cs
@@ -1,7 +1,10 @@
+using System.Text;
 using EMGATA.API.Data;
 using EMGATA.API.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,13 +20,14 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 	options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add Identity
-builder.Services.AddIdentity<User, IdentityRole<int>>(options => {
-		options.Password.RequireDigit = true;
-		options.Password.RequireLowercase = true;
-		options.Password.RequireUppercase = true;
-		options.Password.RequireNonAlphanumeric = true;
-		options.Password.RequiredLength = 6;
-	})
+builder.Services.AddIdentity<User, IdentityRole<int>>(options =>
+{
+	options.Password.RequireDigit = true;
+	options.Password.RequireLowercase = true;
+	options.Password.RequireUppercase = true;
+	options.Password.RequireNonAlphanumeric = true;
+	options.Password.RequiredLength = 6;
+})
 	.AddEntityFrameworkStores<ApplicationDbContext>()
 	.AddDefaultTokenProviders();
 
@@ -31,6 +35,26 @@ builder.Services.AddIdentity<User, IdentityRole<int>>(options => {
 builder.Services.AddScoped<UserManager<User>>();
 builder.Services.AddScoped<SignInManager<User>>();
 
+// Configurer l’authentification JWT
+builder.Services.AddAuthentication(options =>
+{
+	options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+	options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+	options.TokenValidationParameters = new TokenValidationParameters
+	{
+		ValidateIssuer = true,
+		ValidateAudience = true,
+		ValidateLifetime = true,
+		ValidateIssuerSigningKey = true,
+		ValidIssuer = builder.Configuration["Jwt:Issuer"],
+		ValidAudience = builder.Configuration["Jwt:Audience"],
+		IssuerSigningKey = new SymmetricSecurityKey(
+					Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"] ?? ""))
+	};
+
+});
 
 
 var app = builder.Build();
@@ -38,8 +62,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+	app.UseSwagger();
+	app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
