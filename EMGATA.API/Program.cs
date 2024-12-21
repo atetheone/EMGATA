e7e4024a4a77e3ae0bd52a69f4e2c0a30e0d7e55
atetheone <atevirran@gmail.com>
2024-12-21 12:04:49 +0000

    Add Identity configuration with password requirements and token providers

--- EMGATA.API/Program.cs
+++ EMGATA.API/Program.cs
@@ -1,4 +1,6 @@
 using EMGATA.API.Data;
+using EMGATA.API.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,6 +15,22 @@ builder.Services.AddSwaggerGen();
 // Add SQLite DbContext
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 	options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Add Identity
+builder.Services.AddIdentity<User, IdentityRole<int>>(options => {
+		options.Password.RequireDigit = true;
+		options.Password.RequireLowercase = true;
+		options.Password.RequireUppercase = true;
+		options.Password.RequireNonAlphanumeric = true;
+		options.Password.RequiredLength = 6;
+	})
+	.AddEntityFrameworkStores<ApplicationDbContext>()
+	.AddDefaultTokenProviders();
+
+
+builder.Services.AddScoped<UserManager<User>>();
+builder.Services.AddScoped<SignInManager<User>>();
+
 
 
 var app = builder.Build();
