50782c4a5d17e4a951bd38e4539c2e90bbb96b91
atetheone <atevirran@gmail.com>
2024-12-22 12:19:03 +0000

    Add ICarImageService interface for managing car images

---
+++ EMGATA.API/Services/ICarImageService.cs
@@ -0,0 +1,13 @@
+﻿using EMGATA.API.Models;
+
+namespace EMGATA.API.Services;
+
+public interface ICarImageService
+{
+	Task<IEnumerable<CarImage>> GetImagesByCarAsync(int carId);
+	Task<CarImage> GetMainImageByCarAsync(int carId);
+	Task<CarImage> AddImageAsync(int carId, string imageUrl, bool isMain = false);
+	Task DeleteImageAsync(int imageId);
+	Task SetMainImageAsync(int carId, int imageId);
+  Task<CarImage> GetImageByIdAsync(int imageId);
+}
