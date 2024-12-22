443ccd76afce302d036e8f05d39d151e406bc016
atetheone <atevirran@gmail.com>
2024-12-22 12:16:58 +0000

    Add ModelRepository implementation with methods for retrieving models by brand

---
+++ EMGATA.API/Repositories/impl/ModelRepository.cs
@@ -0,0 +1,24 @@
+﻿using EMGATA.API.Data;
+using EMGATA.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EMGATA.API.Repositories;
+
+public class ModelRepository : GenericRepository<Model>, IModelRepository
+{
+	public ModelRepository(ApplicationDbContext context) : base(context) { }
+
+	public async Task<IEnumerable<Model>> GetModelsByBrandAsync(int brandId)
+	{
+		return await _context.Models
+			.Where(m => m.BrandId == brandId)
+			.ToListAsync();
+	}
+
+	public async Task<Model>? GetModelWithBrandAsync(int id)
+	{
+		return await _context.Models
+			.Include(m => m.Brand)
+			.FirstOrDefaultAsync(m => m.Id == id);
+	}
+}
