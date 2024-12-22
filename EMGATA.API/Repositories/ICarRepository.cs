c4b4ab0ad24cf3cd7af25d8717180e4703d9b0c5
atetheone <atevirran@gmail.com>
2024-12-22 12:15:56 +0000

    Add ICarRepository interface with methods for car retrieval and filtering

---
+++ EMGATA.API/Repositories/ICarRepository.cs
@@ -0,0 +1,11 @@
+﻿using EMGATA.API.Models;
+namespace EMGATA.API.Repositories;
+
+public interface ICarRepository: IGenericRepository<Car>
+{
+	Task<IEnumerable<Car>> GetAvailableCarsAsync();
+	Task<Car> GetCarWithDetailsAsync(int id);
+	Task<IEnumerable<Car>> GetCarsByBrandAsync(int brandId);
+	Task<IEnumerable<Car>> GetCarsByModelAsync(int modelId);
+	Task<IEnumerable<Car>> GetCarsByUserAsync(int userId);
+}
