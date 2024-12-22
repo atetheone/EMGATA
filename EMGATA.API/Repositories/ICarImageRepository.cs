88e0ea6815d8fb715b29f275394a13ffaa69d1a8
atetheone <atevirran@gmail.com>
2024-12-22 12:16:26 +0000

    Add ICarImageRepository interface with methods for retrieving and managing car images

---
+++ EMGATA.API/Repositories/ICarImageRepository.cs
@@ -0,0 +1,10 @@
+﻿using EMGATA.API.Models;
+
+namespace EMGATA.API.Repositories;
+
+public interface ICarImageRepository : IGenericRepository<CarImage>
+{
+	Task<IEnumerable<CarImage>> GetImagesByCarAsync(int carId);
+	Task<CarImage> GetMainImageByCarAsync(int carId);
+	Task SetPrimaryImageAsync(int carId, int imageId);
+}
