3d15bc00119885d56547d70b1390e6d048b23b96
atetheone <atevirran@gmail.com>
2024-12-22 12:18:50 +0000

    Add IModelService interface for managing model operations

---
+++ EMGATA.API/Services/IModelService.cs
@@ -0,0 +1,13 @@
+﻿using EMGATA.API.Models;
+
+namespace EMGATA.API.Services;
+
+public interface IModelService
+{
+	Task<IEnumerable<Model>> GetAllModelsAsync();
+	Task<Model> GetModelByIdAsync(int id);
+	Task<IEnumerable<Model>> GetModelsByBrandAsync(int brandId);
+	Task<Model> CreateModelAsync(Model model);
+	Task UpdateModelAsync(Model model);
+	Task DeleteModelAsync(int id);
+}
