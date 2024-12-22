de9d8f93ebc9959293ea475e37d5aeedb571123b
atetheone <atevirran@gmail.com>
2024-12-22 12:19:24 +0000

    Add ICarService interface for managing car operations

---
+++ EMGATA.API/Services/ICarService.cs
@@ -0,0 +1,18 @@
+﻿using EMGATA.API.Dtos;
+using EMGATA.API.Models;
+using EMGATA.API.Models.enums;
+
+namespace EMGATA.API.Services
+{
+	public interface ICarService
+	{
+		Task<IEnumerable<Car>> GetAllCarsAsync();
+		Task<IEnumerable<Car>> GetAvailableCarsAsync();
+		Task<Car> GetCarByIdAsync(int id);
+		Task<Car> AddCarAsync(Car car);
+		Task UpdateCarAsync(Car car);
+		Task DeleteCarAsync(int id);
+		Task<Car> UpdateCarStatusAsync(int id, CarStatus status);
+		Task<IEnumerable<Car>> GetCarsByUserAsync(int userId);
+	}
+}
