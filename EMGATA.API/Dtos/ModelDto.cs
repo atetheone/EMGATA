3c7d92d66784daf8714cdf6c9ad2ec3ce02403c9
atetheone <atevirran@gmail.com>
2024-12-22 12:21:39 +0000

    Add ModelDto, CreateModelDto, and UpdateModelDto for model management

---
+++ EMGATA.API/Dtos/ModelDto.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EMGATA.API.Dtos;
+
+public class ModelDto
+{
+	public int Id { get; set; }
+	public int BrandId { get; set; }
+	public string Name { get; set; }
+	public string Description { get; set; }
+	public string BrandName { get; set; }
+}
+
+public class CreateModelDto
+{
+	[Required(ErrorMessage = "Brand ID is required")]
+	public int BrandId { get; set; }
+
+	[Required(ErrorMessage = "Model name is required")]
+	[StringLength(100, ErrorMessage = "Model name cannot be longer than 100 characters")]
+	public string Name { get; set; }
+
+	[StringLength(500, ErrorMessage = "Description cannot be longer than 500 characters")]
+	public string Description { get; set; }
+}
+
+public class UpdateModelDto
+{
+	[Required(ErrorMessage = "Brand ID is required")]
+	public int BrandId { get; set; }
+
+	[Required(ErrorMessage = "Model name is required")]
+	[StringLength(100, ErrorMessage = "Model name cannot be longer than 100 characters")]
+	public string Name { get; set; }
+
+	[StringLength(500, ErrorMessage = "Description cannot be longer than 500 characters")]
+	public string Description { get; set; }
+}
