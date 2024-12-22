7b25af63bd9eb6442a50a460a0748eb3b2f38c44
atetheone <atevirran@gmail.com>
2024-12-22 12:19:47 +0000

    Add BrandService implementation for managing brand operations

---
+++ EMGATA.API/Services/impl/BrandService.cs
@@ -0,0 +1,44 @@
+﻿using EMGATA.API.Models;
+using EMGATA.API.Repositories;
+
+namespace EMGATA.API.Services;
+
+public class BrandService : IBrandService
+{
+	private readonly IBrandRepository _brandRepository;
+
+	public BrandService(IBrandRepository brandRepository)
+	{
+		_brandRepository = brandRepository;
+	}
+
+	public async Task<IEnumerable<Brand>> GetAllBrandsAsync()
+	{
+		return await _brandRepository.GetAllAsync();
+	}
+
+	public async Task<Brand> GetBrandByIdAsync(int id)
+	{
+		return await _brandRepository.GetByIdAsync(id);
+	}
+
+	public async Task<Brand> GetBrandWithModelsAsync(int id)
+	{
+		return await _brandRepository.GetBrandWithModelsAsync(id);
+	}
+
+	public async Task<Brand> CreateBrandAsync(Brand brand)
+	{
+		return await _brandRepository.AddAsync(brand);
+	}
+
+	public async Task UpdateBrandAsync(Brand brand)
+	{
+		await _brandRepository.UpdateAsync(brand);
+	}
+
+	public async Task DeleteBrandAsync(int id)
+	{
+		await _brandRepository.DeleteAsync(id);
+	}
+}
