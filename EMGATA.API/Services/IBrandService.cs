437ed55dcd2e1980dcbdb13383f6e52fadf4d1f7
atetheone <atevirran@gmail.com>
2024-12-22 12:18:30 +0000

    Add IBrandService interface for managing brand operations

---
+++ EMGATA.API/Services/IBrandService.cs
@@ -0,0 +1,13 @@
+﻿using EMGATA.API.Models;
+
+namespace EMGATA.API.Services;
+
+public interface IBrandService
+{
+	Task<IEnumerable<Brand>> GetAllBrandsAsync();
+	Task<Brand> GetBrandByIdAsync(int id);
+	Task<Brand> GetBrandWithModelsAsync(int id);
+	Task<Brand> CreateBrandAsync(Brand brand);
+	Task UpdateBrandAsync(Brand brand);
+	Task DeleteBrandAsync(int id);
+}
