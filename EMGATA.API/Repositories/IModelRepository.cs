4add10a764b3842360c17c32eb527b88d313afcf
atetheone <atevirran@gmail.com>
2024-12-22 12:16:08 +0000

    Add IModelRepository interface with methods for retrieving models by brand

---
+++ EMGATA.API/Repositories/IModelRepository.cs
@@ -0,0 +1,9 @@
+﻿using EMGATA.API.Models;
+
+namespace EMGATA.API.Repositories;
+
+public interface IModelRepository : IGenericRepository<Model>
+{
+	Task<IEnumerable<Model>> GetModelsByBrandAsync(int brandId);
+	Task<Model> GetModelWithBrandAsync(int id);
+}
