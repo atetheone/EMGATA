93842628827eb2e55fa99567f64d3d665eb09586
atetheone <atevirran@gmail.com>
2024-12-22 12:19:32 +0000

    Add ITokenService interface for managing token generation

---
+++ EMGATA.API/Services/ITokenService.cs
@@ -0,0 +1,9 @@
+﻿using EMGATA.API.Models;
+namespace EMGATA.API.Services;
+
+public interface ITokenService
+{
+	// Task<string> GenerateToken(User user, string role);
+	Task<string> GenerateToken(User user, IList<string> roles);
+
+}
