384c9602102880c64f03690abe08126a0e012514
atetheone <atevirran@gmail.com>
2024-12-22 12:16:45 +0000

    Add BrandRepository implementation with method to retrieve brand and its models

---
+++ EMGATA.API/Repositories/impl/BrandRepository.cs
@@ -0,0 +1,17 @@
+﻿using EMGATA.API.Data;
+using EMGATA.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EMGATA.API.Repositories;
+
+public class BrandRepository : GenericRepository<Brand>, IBrandRepository
+{
+	public BrandRepository(ApplicationDbContext context) : base(context) { }
+
+	public async Task<Brand> GetBrandWithModelsAsync(int id)
+	{
+		return await _context.Brands
+			.Include(b => b.Models)
+			.FirstOrDefaultAsync(b => b.Id == id);
+	}
+}
