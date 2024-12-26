0b00f29668ee43a4467f519635038c0c1d407050
atetheone <atevirran@gmail.com>
2024-12-26 18:58:36 +0000

    Refactor IBrandService to remove GetBrandWithModelsAsync method for improved interface clarity

--- EMGATA.API/Services/IBrandService.cs
+++ EMGATA.API/Services/IBrandService.cs
@@ -6,7 +6,6 @@ public interface IBrandService
 {
 	Task<IEnumerable<Brand>> GetAllBrandsAsync();
 	Task<Brand> GetBrandByIdAsync(int id);
-	Task<Brand> GetBrandWithModelsAsync(int id);
 	Task<Brand> CreateBrandAsync(Brand brand);
 	Task UpdateBrandAsync(Brand brand);
 	Task DeleteBrandAsync(int id);
