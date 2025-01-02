4e16d730dbd1dc1fdcfbb00f4aa3326b346dbf8c
atetheone <atevirran@gmail.com>
2025-01-02 22:47:12 +0000

    refactor: update AuthResponseDto to make Token nullable and remove it from success response

--- EMGATA.API/Controllers/AuthController.cs
+++ EMGATA.API/Controllers/AuthController.cs
@@ -67,7 +67,6 @@ public class AuthController : ControllerBase
         {
             IsSuccess = true,
             Message = "User created successfully",
-            Token = token
         });
     }
 
--- EMGATA.API/Dtos/AuthDto.cs
+++ EMGATA.API/Dtos/AuthDto.cs
@@ -32,6 +32,6 @@ public class AuthResponseDto
 {
 	public bool IsSuccess { get; set; }
 	public string Message { get; set; }
-	public string Token { get; set; }
+	public string? Token { get; set; }
 	public DateTime? Expiration { get; set; }
 }
