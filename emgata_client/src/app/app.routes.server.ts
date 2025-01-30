009e46cd9badd285be45071f235d2a60b5173cb0
atetheone <atevirran@gmail.com>
2025-01-30 21:25:24 +0000

    feat: update server routes to use prerendering for car details and admin edit pages

--- emgata_client/src/app/app.routes.server.ts
+++ emgata_client/src/app/app.routes.server.ts
@@ -1,20 +1,50 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
+import { CarService } from './core/services/car.service';
+import { inject } from '@angular/core';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: '',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'about',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'contact',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'cars',
+    renderMode: RenderMode.Prerender
+  },
+  // Dynamic routes with parameters - use Server rendering
+  {
     path: 'cars/:id',
-    renderMode: RenderMode.Dynamic
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      const carService = inject(CarService);
+      const cars = await carService.getAllCars().toPromise();
+      return cars!.map(car => ({ id: car.id.toString() }));
+      // This will prerender paths like: /cars/1, /cars/2, etc.
+    },
   },
   {
     path: 'admin/cars/:id/edit',
-    renderMode: RenderMode.Dynamic
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      const carService = inject(CarService);
+      const cars = await carService.getAllCars().toPromise();
+      return cars!.map(car => ({ id: car.id.toString() }));
+    },
+  },
+  {
+    path: 'admin/**',
+    renderMode: RenderMode.Server
   },
   {
     path: '**',
-    renderMode: RenderMode.Dynamic
-  },
-  {
-    path: 'home',
     renderMode: RenderMode.Prerender
   }
 ];
